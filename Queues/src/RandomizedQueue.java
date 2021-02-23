7f304d43cc79b3ee47594a886d486efe71f4174f
haseefathi <hasee.fathi@gmail.com>
2021-02-23 19:56:22 +0400

    Adding queues

--- Queues/src/RandomizedQueue.java
+++ Queues/src/RandomizedQueue.java
@@ -1,4 +1,8 @@
-public class RandomizedQueue {
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
 
     private Item[] array;
     private int size = 0; // number of items
@@ -6,6 +10,7 @@ public class RandomizedQueue {
     // construct an empty randomized queue
     public RandomizedQueue() {
         array = (Item[]) new Object[1];
+        System.out.println("Object created");
     }
 
     // is the randomized queue empty?
@@ -20,11 +25,107 @@ public class RandomizedQueue {
 
     // add the item
     public void enqueue(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Item to enqueue cannot be null");
+        }
+        if (size == array.length) {
+            resizeArray(array.length * 2);
+        }
+        array[size++] = item;
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException("Cannot dequeue from empty queue!");
+        }
+        int index = getRandomIndex();
+        Item item = array[index];
+        array[index] = array[--size];
+        if (size < (array.length / 4)) {
+            resizeArray(array.length / 2);
+        }
+        return item;
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException("No sample from empty queue!");
+        }
+        return array[getRandomIndex()];
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+
+        private int currentIndex = 0;
+        private Item[] iteratorArray = (Item[]) new Object[size];
+
+        public RandomizedQueueIterator() {
+            for (int i = 0; i < size; i++) {
+                iteratorArray[i] = array[i];
+            }
+            StdRandom.shuffle(iteratorArray);
+        }
+
+        public boolean hasNext() {
+            return currentIndex < size;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("Cannot invoke remove operation");
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException("No next element!");
+            }
+            Item item = iteratorArray[currentIndex];
+            currentIndex++;
+            return item;
+        }
+
 
     }
 
+
+    private int getRandomIndex() {
+        return StdRandom.uniform(size);
+    }
+
     private void resizeArray(int newLength) {
-        
+        Item[] newArray = (Item[]) new Object[newLength];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+
+    public static void main(String[] args) {
+        RandomizedQueue<Integer> q = new RandomizedQueue<Integer>();
+        System.out.println(q.size());
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+        q.enqueue(11);
+        q.enqueue(12);
+        q.enqueue(13);
+        q.enqueue(14);
+        System.out.println(q.size());
+        System.out.println(q.dequeue());
+        System.out.println(q.size());
+        System.out.println(q.sample());
+        System.out.println("Printing all items:");
+        for (int item : q) {
+            System.out.println(item);
+        }
+
     }
 
 }
